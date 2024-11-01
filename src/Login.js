97568b934b0c4c9da17145526d770f0148503749
Diogo Maggio de Azevedo <103466242+diogomaggio@users.noreply.github.com>
2024-11-01 11:12:45 -0300

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './index.css';
 
-function Login({ handleLogin }) {
+function Login({ handleLogin, handleSignup, handleForgotPassword }) {
   return (
     <div className="login-container">
-      <img src="login_logo.png" alt="Logo" className="login-logo" />
-      <h2>Bem-vindo à Insight Company. Gerencie seu dinheiro com sabedoria!</h2>
+      <img src="login_logo.png" alt="Logo" className="login-logo" /> {/* Ajustando a logo aqui */}
+      <h2>Login</h2>
+      <p className="welcome-message">Bem-vindo ao Portal de Finanças. Gerencie seu dinheiro com sabedoria!</p>
       <form onSubmit={(e) => {
         e.preventDefault();
         handleLogin();
@@ -21,7 +22,7 @@ function Login({ handleLogin }) {
         <button type="submit" className="login-button">Entrar</button>
       </form>
       <div className="login-links">
-        <a href="#">Esqueci minha senha</a> | <a href="#">Cadastre-se</a>
+        <a href="#" onClick={handleForgotPassword}>Esqueci minha senha</a> | <a href="#" onClick={handleSignup}>Cadastre-se</a>
       </div>
     </div>
   );
